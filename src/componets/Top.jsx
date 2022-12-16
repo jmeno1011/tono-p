48cf7330774569c8eeabba4c87aebfd5978e705e
jmeno1011 <jwhltn8282@gmail.com>
2022-12-16 13:59:16 +0900

    add styled-components and apply js in css used by babel

---
+++ src/componets/Top.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const HeaderBlcok = styled.header`
+    height: 64px;
+    padding: 0 20px;
+`
+
+const Top = () => {
+  return (
+    <HeaderBlcok>Top입니다.</HeaderBlcok>
+  )
+}
+
+export default Top
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
+import Top from "../componets/Top"
+
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+        <Top />
+        <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
