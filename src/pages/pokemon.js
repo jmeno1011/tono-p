1a70f654dc2a21ec85b9de2b694daea55ac8dcb1
jmeno1011 <jwhltn8282@gmail.com>
2022-12-19 22:23:44 +0900

    add pokemon api

--- src/pages/pokemon.js
+++ src/pages/pokemon.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import axios from "axios"
 
-const pokemon = () => {
+const pokemon = ({pokemons}) => {
   return (
     <div>pokemon</div>
   )
 }
 
 export default pokemon
+
+export async function getStaticProps(){
+  const apiUrl = process.env.POKEMON_API_URL;
+  const res = await axios.get(apiUrl);
+  const data = res.data;
+
+  return{
+    props:{
+      pokemons: data
+    }
+  }
+}
