97dd90f334c13b392492f1c5bad3b4c7f5feb774
menomik1011 <39692905+meno1011@users.noreply.github.com>
2022-12-19 15:25:09 +0900

    add card layout

---
+++ src/componets/commons/ProjectCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const ProjectCardBlock = styled.div`
+    
+`
+
+const ProjectCard = () => {
+  return (
+    <div>ProjectCard</div>
+  )
+}
+
+export default ProjectCard
