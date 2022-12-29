020c9e04bb982a2c3a00fa8bb1e0463770da1392
jmeno1011 <jwhltn8282@gmail.com>
2022-12-29 15:50:32 +0900

    Revert "add firebase and config"
    This reverts commit f71d6d48ecf20d495b60c2e93afd9bdaaebcbed2.

--- src/componets/calendar/ScheduleCard.jsx
+++ src/componets/calendar/ScheduleCard.jsx
@@ -1,27 +1,12 @@
-import { Icon } from '@iconify/react'
 import React from 'react'
 import styled from 'styled-components'
 
 const ScheduleCardBlock = styled.div`
-  padding: 8px;
-  margin-bottom: 6px;
-  background-color: #fff;
-  border-left: 4px solid ${(props) => props.color};
-  overflow: hidden;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-
-  span{
-    font-size: 0.75rem;
-    font-weight: 500;
-    color: rgba(0,0,0,.6);
-  }
-
-  svg{
-    cursor: pointer;
-    font-size: 1.5rem;
-  }
+    padding: 8px;
+    margin-bottom: 6px;
+    background-color: #fff;
+    border-left: 4px solid ${(props)=>props.color};
+    overflow: hidden;
 `
 
 /*
@@ -34,16 +19,10 @@ const ScheduleCardBlock = styled.div`
 }
 */
 
-const ScheduleCard = ({ title, date, color }) => {
+const ScheduleCard = ({title, date, color}) => {
   return (
     <ScheduleCardBlock color={color} className="box-sd">
-      <div>
         <h5>{title}</h5>
-        <span>{date}</span>
-      </div>
-      <div>
-        <Icon icon="material-symbols:keyboard-arrow-down-rounded" />
-      </div>
     </ScheduleCardBlock>
   )
 }
